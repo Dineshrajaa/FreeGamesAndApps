60ad04c0eb51fe3e585a42013b67cb6cee6bf895
Dineshrajaa <dineshrajaa.93@gmail.com>
2015-10-18 19:58:32 +0530

    Completed the basic Template

---
+++ www/js/controllers.js
@@ -0,0 +1,30 @@
+angular.module('starter.controllers', [])
+
+.controller('contactCtrl', function($scope) {
+    /* Page varables */
+    $scope.profile = {};
+    $scope.profile.title = 'Free Games and Apps Ltd.';
+    $scope.profile.subtitle = 'Quality product for all';
+    $scope.profile.phone = '07786324603';
+    $scope.profile.email = 'bhatiar2003@yahoo.co.uk';
+    $scope.profile.website = 'numsiii.com';
+    $scope.profile.description = 'We make quality mobile apps and games for out clients and users';
+})
+
+.controller('infoCtrl', function($scope) {
+
+})
+
+.controller('achievementCtrl', function($scope) {
+
+})
+
+.controller('introductionCtrl', function($scope) {
+
+})
+
+.controller('videosCtrl', function($scope) {
+
+})
+
+
